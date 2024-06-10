d3f4bc2e42f41e9b455a8b79de2c50c33d052ada
Roger BC <roger@chaindots.com>
2024-06-10 15:30:17 -0300

    add: LocalState added

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,11 +1,19 @@
-import { mockTodoList } from '@/utils';
+import { useLocalState } from '@/hooks/useLocalStorage';
+import { Todo } from '@/types';
+import { useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
+import { EmptyTodo } from './Empty';
 import { TodoItem } from './TodoItem';
 
 export const TodoList = () => {
+  const [value, setValue] = useLocalState<Todo[]>('todoList', []);
+  const [todoList, setTodoList] = useState<Todo[]>(value);
+  if (todoList.length === 0) {
+    return <EmptyTodo />;
+  }
   return (
     <ListGroup as="ol" numbered>
-      {mockTodoList(50).map((todo, index) => (
+      {todoList.map((todo, index) => (
         <TodoItem key={index} todo={todo} />
       ))}
     </ListGroup>
