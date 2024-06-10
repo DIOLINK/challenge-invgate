e077b72abc51eb40af3101e6e8c98e10c5980a2d
Roger BC <roger@chaindots.com>
2024-06-10 15:21:56 -0300

    refactor: Search bar

--- src/components/Layout/Navbar/Search.tsx
+++ src/components/Layout/Navbar/Search.tsx
@@ -1,8 +1,23 @@
 import { AddItem } from '@/components';
 import { useUIContext } from '@/contexts/UI/UIProvider';
+import { ChangeEvent } from 'react';
 import { Col, Form, Row } from 'react-bootstrap';
 export const Search = () => {
   const { openModal } = useUIContext();
+
+  const OPTIONS_SELECTED = [
+    { value: 'all', label: 'All Todo' },
+    { value: 'done', label: 'Done' },
+    { value: 'pending', label: 'Pending' },
+  ];
+
+  function handleChange(
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ): void {
+    console.log('🚀 ~ Search ~ event:', event.target.value);
+
+  }
+
   return (
     <Col md={8}>
       <Form className="flex-grow-1">
@@ -12,17 +27,28 @@ export const Search = () => {
               type="text"
               placeholder="Search"
               className=" mr-sm-2"
+              autoComplete="off"
+              name="search"
+              onChange={(event) =>
+                handleChange(event as ChangeEvent<HTMLInputElement>)
+              }
             />
           </Col>
           <Col xs="3">
-            <Form.Check
-              type="switch"
-              id="custom-switch"
-              label="Pending Search"
-              className="d-flex align-items-center justify-content-around pointer"
-            />
+            <Form.Select
+              aria-label="Default select example"
+              name="selectFilter"
+              onChange={(event) =>
+                handleChange(event as ChangeEvent<HTMLSelectElement>)
+              }
+            >
+              {OPTIONS_SELECTED.map((option, index) => (
+                <option key={option.value + index} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Form.Select>
           </Col>
-          <AddItem variant="primary" btnTitle="Search" iconStart={null} />
           <AddItem onClick={openModal} />
         </Row>
       </Form>
